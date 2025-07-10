e77e3b8a2fc649efbb62f24d1426c898bd5b411a
Sourya Mishra <souryamishra@Souryas-MacBook-Pro.local>
2025-07-10 09:25:45 +0530

    implement add to cart feature with server actions

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,64 @@
+import { prisma } from "@/lib/db/prisma";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import PriceTag from "@/components/PriceTag";
+import { Metadata } from "next";
+import { cache } from "react";
+import AddToCartButton from "./AddToCartButton";
+import { incrementProductQuantity } from "./actions";
+
+interface IProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (!product) notFound();
+  return product;
+});
+
+export async function generateMetadata({
+  params: { id },
+}: IProductPageProps): Promise<Metadata> {
+  const product = await getProduct(id);
+
+  return {
+    title: product.name + " - Lamazon",
+    description: product.description,
+    openGraph: {
+      images: [{ url: product.imageUrl }],
+    },
+  };
+}
+
+export default async function ProductPage({
+  params: { id },
+}: IProductPageProps) {
+  const product = await getProduct(id);
+
+  return (
+    <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={500}
+        height={500}
+        className="rounded-lg"
+        priority
+      />
+
+      <div>
+        <h1 className="text-5xl font-bold">{product.name}</h1>
+        <PriceTag price={product.price} className="mt-4" />
+        <p className="py-6">{product.description}</p>
+        {/* Pass the server action from Server component to Client component */}
+        <AddToCartButton
+          productId={product.id}
+          incrementProductQuantity={incrementProductQuantity}
+        />
+      </div>
+    </div>
+  );
+}
