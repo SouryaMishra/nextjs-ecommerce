298c693f2440c9076f33e1fa0979cfa3288a309f
Sourya Mishra <sourya.mishra111@gmail.com>
2025-07-15 15:18:46 +0530

    remove export of authOptions from route.ts and fix build error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,5 @@
-import { mergeAnonymousCartWithUserCart } from "@/lib/db/cart";
-import { prisma } from "@/lib/db/prisma";
-import { env } from "@/lib/env";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma as PrismaClient),
-  providers: [
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    session({ session, user }) {
-      // Set the user id on session after fetching it from db
-      session.user.id = user.id;
-      return session;
-    },
-  },
-  events: {
-    // Merge carts just after sign in and before redirecting to home page
-    async signIn({ user }) {
-      await mergeAnonymousCartWithUserCart(user.id);
-    },
-  },
-};
+import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 
--- src/components/FormSubmitButton.tsx
+++ src/components/FormSubmitButton.tsx
@@ -1,27 +1,18 @@
 "use client";
 
 import { ComponentProps } from "react";
-import { useFormStatus } from "react-dom";
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
 
 interface IFormSubmitButtonProps extends ComponentProps<"button"> {
   children: React.ReactNode;
   className?: string;
 }
 
-export default function FormSubmitButton({
-  children,
-  className,
-  ...props
-}: IFormSubmitButtonProps) {
+export default function FormSubmitButton({ children, className, ...props }: IFormSubmitButtonProps) {
   const { pending } = useFormStatus();
 
   return (
-    <button
-      {...props}
-      type="submit"
-      className={`btn-primary btn ${className}`}
-      disabled={pending}
-    >
+    <button {...props} type="submit" className={`btn-primary btn ${className}`} disabled={pending}>
       {pending && <span className="loading loading-spinner" />}
       {children}
     </button>
