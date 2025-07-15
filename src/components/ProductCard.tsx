af9f0dfb0bc8459a6a3edc728fc520ed6f1557a6
Sourya Mishra <sourya.mishra111@gmail.com>
2025-07-15 15:19:34 +0530

    format files

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -8,23 +8,12 @@ interface IProductCardProps {
 }
 
 export default function ProductCard({ product }: IProductCardProps) {
-  const isNew =
-    Date.now() - new Date(product.createdAt).getTime() <
-    1000 * 60 * 60 * 24 * 7; // i.e. 7 days
+  const isNew = Date.now() - new Date(product.createdAt).getTime() < 1000 * 60 * 60 * 24 * 7; // i.e. 7 days
 
   return (
-    <Link
-      href={"/products/" + product.id}
-      className="card w-full bg-base-100 transition hover:shadow-xl"
-    >
+    <Link href={"/products/" + product.id} className="card w-full bg-base-100 transition hover:shadow-xl">
       <figure>
-        <Image
-          src={product.imageUrl}
-          alt={product.name}
-          width={800}
-          height={400}
-          className="h-40 object-cover"
-        />
+        <Image src={product.imageUrl} alt={product.name} width={800} height={400} className="h-40 object-cover" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{product.name}</h2>
